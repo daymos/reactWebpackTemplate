3e773b2e9d6dfaafd9a9e9581d9dc1c18c64adaf
mattiaS <mattia@3doers.it>
2016-08-04 09:39:44 +0100

    chore(setupRepo): add package and webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports={
+
+  context: __dirname,
+  entry:'./index.js',
+  output:{
+    path:'./',
+    filename:'bundle.js'
+  },
+  devServer:{
+    inline: true,
+    port:3333
+  },
+  devtool: 'source-map',
+  module:{
+  loaders:[
+    {
+      test: /\.js$/,
+      exclude:/node_modules/,
+      loader:'babel',
+      query:{
+        presets:['es2015','react']}
+    }] 
+  }
+
+}
